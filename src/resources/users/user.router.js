163842c5f004787913ed57b8918021d1b206bce7
German Bilalov <pinncho@gmail.com>
2021-12-01 20:44:07 +0600

    feat: add user router

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,10 @@
-const router = require('express').Router();
-const User = require('./user.model');
-const usersService = require('./user.service');
+const router = require('@koa/router')();
+const userService = require('./user.service.js');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router.post('/users', userService.create);
+router.get('/users', userService.getAll);
+router.get('/users/:userID', userService.getOne);
+router.put('/users/:userID', userService.updateOne);
+router.delete('/users/:userID', userService.deleteOne);
 
 module.exports = router;
