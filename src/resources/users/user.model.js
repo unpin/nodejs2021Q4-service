1d1b2d56923dc4bd95a0f1206f030322cd39198f
German Bilalov <pinncho@gmail.com>
2021-12-01 20:07:44 +0600

    feat: add User model

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,22 +1,50 @@
-const uuid = require('uuid');
+const schemaValidator = require('../../database/schema/schemaValidator');
+const UUID = require('../../utils/uuid/UUID');
 
 class User {
-  constructor({
-    id = uuid(),
-    name = 'USER',
-    login = 'user',
-    password = 'P@55w0rd'
-  } = {}) {
-    this.id = id;
-    this.name = name;
-    this.login = login;
-    this.password = password;
+  constructor(data) {
+    Object.assign(this, User.trimObject(data));
   }
 
   static toResponse(user) {
-    const { id, name, login } = user;
-    return { id, name, login };
+    const { password, ...publicUser } = user;
+    return publicUser;
+  }
+
+  static trimObject(data) {
+    const trimmed = {};
+    Object.keys(User.schema).forEach((prop) => {
+      if (prop in data) {
+        trimmed[prop] = data[prop];
+      }
+    });
+    return trimmed;
+  }
+
+  validate() {
+    schemaValidator(User, this);
   }
 }
+
+User.schema = {
+  id: {
+    type: UUID,
+  },
+  name: {
+    type: String,
+    minlen: 1,
+    required: true,
+  },
+  login: {
+    type: String,
+    minlen: 1,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlen: 1,
+    required: true,
+  },
+};
 
 module.exports = User;
