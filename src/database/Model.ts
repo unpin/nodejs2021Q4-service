9f3d70e15f13de6a93fdb739f781a15f3b5e17c7
German Bilalov <pinncho@gmail.com>
2021-12-19 23:35:01 +0600

    ts-docs: add documentation for Model.validate()

--- src/database/Model.ts
+++ src/database/Model.ts
@@ -1,5 +1,14 @@
 export default abstract class Model {
   id?: string;
 
+  /**
+   *
+   * Method should validate the model data
+   *
+   * @throws {@link SchemaValidationError} if model data is not valid
+   * @returns Returns void
+   *
+   */
+
   abstract validate(): void;
 }
