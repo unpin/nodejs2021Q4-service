6440fe56f6890477fcfdc0ace2282903bf027317
German Bilalov <pinncho@gmail.com>
2021-12-20 00:08:57 +0600

    ts-docs: document SchemaValidationError class

--- src/database/error/SchemaValidationError.ts
+++ src/database/error/SchemaValidationError.ts
@@ -1,4 +1,12 @@
 export default class SchemaValidationError extends Error {
+  /**
+   *
+   * Creates an instance of SchemaValidationError.
+   *
+   * @param message - string message
+   * @returns {@link SchemaValidationError}
+   *
+   */
   constructor(message: string) {
     super(message);
     this.name = SchemaValidationError.name;
