14e828a543de39f15c9644d46587acf642eac981
German Bilalov <pinncho@gmail.com>
2021-12-24 19:29:42 +0600

    feat: set log level of Console and File logger to INFO

--- src/setupLogger.ts
+++ src/setupLogger.ts
@@ -11,11 +11,13 @@ Logger.createLogger({
   transports: [
     new Logger.transports.ConsoleTransport({
       name: 'ConsoleLogger',
+      level: Logger.LEVEL.ALL,
       timestamp: true,
       dateTimeFormat: 'YYYY-MM-DD HH:mm:ss:SSS',
     }),
     new Logger.transports.FileTransport({
       name: 'FileLogger',
+      level: Logger.LEVEL.ALL,
       filename: './logs/misc.log',
       timestamp: true,
     }),
