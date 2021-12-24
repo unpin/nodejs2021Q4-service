711def84edd9184f8c41b5e94ea81a7982903800
German Bilalov <pinncho@gmail.com>
2021-12-24 18:40:43 +0600

    refactor: use Logger to log application start

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { PORT } from './common/config';
 import app from './app';
+import { Logger } from './lib/logger/Logger';
 
 app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
+  Logger.info(`App is running on http://localhost:${PORT}`)
 );
