155788e10a33d90399e34048304d6b01435b6a23
German Bilalov <pinncho@gmail.com>
2022-01-21 04:46:36 +0600

    feat: use isAuthorized middleware in routes

--- src/resources/tasks/task.router.ts
+++ src/resources/tasks/task.router.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
 import * as taskService from './task.service';
+import { isAuthorized } from '../../middleware/isAuthorized';
 
 const router: Router = Router();
 
-router.post('/boards/:boardID/tasks', taskService.create);
-router.get('/boards/:boardID/tasks', taskService.getAll);
-router.get('/boards/:boardID/tasks/:taskID', taskService.getOne);
-router.put('/boards/:boardID/tasks/:taskID', taskService.findByIdAndUpdate);
-router.delete('/boards/:boardID/tasks/:taskID', taskService.findByIdAndDelete);
+router.post('/boards/:boardID/tasks', isAuthorized, taskService.create);
+router.get('/boards/:boardID/tasks', isAuthorized, taskService.getAll);
+router.get('/boards/:boardID/tasks/:taskID', isAuthorized, taskService.getOne);
+router.put(
+  '/boards/:boardID/tasks/:taskID',
+  isAuthorized,
+  taskService.findByIdAndUpdate
+);
+router.delete(
+  '/boards/:boardID/tasks/:taskID',
+  isAuthorized,
+  taskService.findByIdAndDelete
+);
 
 export default router;
