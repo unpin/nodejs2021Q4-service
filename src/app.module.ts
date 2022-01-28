c8eeb611654589896768f00d39c51d3a1dd4dc59
German Bilalov <pinncho@gmail.com>
2022-01-28 06:25:59 +0600

    feat: import TypeOrmModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import {
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+} from './common/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: POSTGRES_HOST,
+      port: Number(POSTGRES_PORT),
+      username: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+      migrationsRun: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
