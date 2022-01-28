6967b997d5b806dd67404d79b05e1297c05bb7db
German Bilalov <pinncho@gmail.com>
2022-01-29 04:41:28 +0600

    feat: enable CORS in the app

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { PORT } from './common/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   await app.listen(PORT);
 }
 bootstrap();
