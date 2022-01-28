1fd9f9bc632657a1efa09e8429a3793e70c44091
German Bilalov <pinncho@gmail.com>
2022-01-28 06:06:05 +0600

    feat: implement UserController

---
+++ src/users/user.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Put,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(@Query() paginationQuery: { limit: number; offset: number }) {
+    const { limit, offset } = paginationQuery;
+    return this.userService.findAll();
+  }
+
+  @Get(':userId')
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('userId') userId: string) {
+    return this.userService.findOne(userId);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userService.create(createUserDto);
+    return User.toResponse(user);
+  }
+
+  @Put(':userId')
+  @HttpCode(HttpStatus.OK)
+  update(
+    @Param('userId') userId: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.update(userId, updateUserDto);
+  }
+
+  @Delete(':userId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('userId') userId: string) {
+    return this.userService.remove(userId);
+  }
+}
