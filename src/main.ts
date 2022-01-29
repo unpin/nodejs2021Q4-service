717f10515409ad6e7d007fcd05f7174e40e3ff8d
German Bilalov <pinncho@gmail.com>
2022-01-30 03:26:36 +0600

    feat: add support for Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PORT } from './common/config';
+import { USE_FASTIFY, PORT } from './common/config';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let app;
+
+  if (USE_FASTIFY === 'true') {
+    app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+    );
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
 
   app.enableCors();
 
-  await app.listen(PORT);
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
