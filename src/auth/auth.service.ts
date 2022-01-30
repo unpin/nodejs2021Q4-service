df77586946ad8cc3f9b4faa924cbd8963c079de7
German Bilalov <pinncho@gmail.com>
2022-01-30 06:21:25 +0600

    feat: implement AuthService

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import { AuthDto } from './dto/auth-dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async signIn(authDto: AuthDto) {
+    const user = await this.findOne(authDto.login);
+    if (!user.passwordsMatch(authDto.password)) {
+      throw new ForbiddenException('Login or password is incorrect');
+    }
+    const token = user.generateJWTToken();
+    return { token };
+  }
+
+  private async findOne(login: string): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: { login },
+    });
+    if (!user) {
+      throw new ForbiddenException('Login or password is incorrect');
+    }
+    return user;
+  }
+}
