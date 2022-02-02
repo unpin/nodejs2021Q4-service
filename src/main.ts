1c5afa2ae290510e8789d3d93790037a5b7a448a
German Bilalov <pinncho@gmail.com>
2022-02-02 08:27:35 +0600

    feat: validate DTOs with ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,16 @@ async function bootstrap() {
   } else {
     app = await NestFactory.create(AppModule);
   }
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   await app.listen(process.env.PORT, '0.0.0.0');
