b8b14805ff77b12330c7c4be394d35e772e20fa0
German Bilalov <pinncho@gmail.com>
2022-02-06 23:20:05 +0600

    feat: add ormconfig.ts

---
+++ ormconfig.ts
@@ -0,0 +1,20 @@
+import 'dotenv';
+
+export default {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  migrationsRun: true,
+  synchronize: false,
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
