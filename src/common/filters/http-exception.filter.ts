5540640d4707afbc0554481b2ab57a0ed581d101
German Bilalov <pinncho@gmail.com>
2022-02-06 12:23:42 +0600

    feat: log http exceptions

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -3,18 +3,36 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  Logger,
 } from '@nestjs/common';
+import { getRequestLoggingMessage } from '../logging/utils';
 
 @Catch(HttpException)
 export class HttpExceptionFilter<T extends HttpException>
   implements ExceptionFilter
 {
   catch(exception: T, host: ArgumentsHost) {
+    const now = Date.now();
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
 
     const status = exception.getStatus();
     const exceptionResponse = exception.getResponse();
+    const res = host.switchToHttp().getResponse();
+    res.on('finish', () => {
+      const { method, url, query, body } = host.switchToHttp().getRequest();
+      const duration = `${Date.now() - now}ms`;
+      const message = getRequestLoggingMessage({
+        method,
+        url,
+        query,
+        body,
+        statusCode: res.statusCode,
+        duration,
+      });
+      Logger.verbose(message);
+    });
+
     const error =
       typeof response === 'string'
         ? {
